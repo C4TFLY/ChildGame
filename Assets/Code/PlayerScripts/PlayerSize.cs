6c6464e4f9c65669aa2f1322060b1af0610ee2dd
Linus Holmström <linus.holmstrom@hotmail.com>
2018-03-13 11:48:22 +0100

    Player size stuff

--- Assets/Code/PlayerScripts/PlayerSize.cs
+++ Assets/Code/PlayerScripts/PlayerSize.cs
@@ -5,15 +5,34 @@ using UnityEngine;
 public class PlayerSize : MonoBehaviour {
 
     public static int size = 1;
+    public float smoothTime = 0.3f;
     public int[] sizeThresholds;
 
     public static int Size { get { return size; } }
 
     private float scaleIncrease;
+    private bool changeScale = false;
+    private float scaleYChange = 0.0f;
+    private float scaleXChange = 0.0f;
 
     private void Start()
     {
         scaleIncrease = 2f / sizeThresholds.Length;
+    }
+
+    private void Update()
+    {
+        if (changeScale)
+        {
+            Vector3 newScale = new Vector3(transform.localScale.x + scaleIncrease,
+                                            transform.localScale.y + scaleIncrease,
+                                            transform.localScale.z);
+            float newScaleY = Mathf.SmoothDamp(transform.localScale.y, newScale.y, ref scaleYChange, smoothTime);
+            float newScaleX = Mathf.SmoothDamp(transform.localScale.x, newScale.x, ref scaleXChange, smoothTime);
+            transform.localScale = new Vector3(newScaleX,
+                                                newScaleY,
+                                                transform.localScale.z);
+        }
     }
 
     public void SizeIncrease()
@@ -22,10 +41,8 @@ public class PlayerSize : MonoBehaviour {
         {
             if (Scoring.PlayerScore > sizeThresholds[size - 1])
             {
+                changeScale = true;
                 size++;
-                transform.localScale = new Vector3(transform.localScale.x + scaleIncrease,
-                                                    transform.localScale.y + scaleIncrease,
-                                                    transform.localScale.z);
             }
         }
     }
