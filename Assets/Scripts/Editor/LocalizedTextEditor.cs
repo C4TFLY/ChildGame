809d77b283a4461eadf540bbaa3ad79222ba9270
Linus Holmström <linus.holmstrom@hotmail.com>
2018-04-24 00:31:26 +0200

    Some more custom editor stuff

--- Assets/Scripts/Editor/LocalizedTextEditor.cs
+++ Assets/Scripts/Editor/LocalizedTextEditor.cs
@@ -8,16 +8,35 @@ public class LocalizedTextEditor : EditorWindow {
 
     public LocalizationData localizationData;
 
+    private Vector2 scrollPos = Vector2.zero;
+    private float currentScrollViewWidth, currentScrollViewHeight;
+    private Rect cursorChangeRect;
+    private bool resize = false;
+
     [MenuItem("Window/Localized Text Editor")]
     private static void Init()
     {
         GetWindow(typeof(LocalizedTextEditor)).Show();
     }
 
+    private void OnEnable()
+    {
+        position = new Rect(200, 200, 400, 300);
+        currentScrollViewWidth = position.width / 2;
+        currentScrollViewHeight = position.height / 2;
+        cursorChangeRect = new Rect(position.width / 2, 0, 5f, position.height);
+    }
+
     private void OnGUI()
     {
+        GUILayout.BeginHorizontal();
+
+        scrollPos = GUILayout.BeginScrollView(scrollPos, GUILayout.Height(currentScrollViewHeight), GUILayout.Width(currentScrollViewWidth));
+        GUILayout.EndScrollView();
+
         if (localizationData != null)
         {
+
             SerializedObject serializedObject = new SerializedObject(this);
             SerializedProperty serializedProperty = serializedObject.FindProperty("localizationData");
             EditorGUILayout.PropertyField(serializedProperty, true);
@@ -37,10 +56,17 @@ public class LocalizedTextEditor : EditorWindow {
         {
             CreateNewData();
         }
+
+        ResizeScrollView();
+        GUILayout.FlexibleSpace();
+
         if(GUILayout.Button("Load comparison data"))
         {
             CompareData();
         }
+
+        GUILayout.EndHorizontal();
+        Repaint();
     }
 
     private void CreateNewData()
@@ -75,11 +101,31 @@ public class LocalizedTextEditor : EditorWindow {
     {
         GetWindow(typeof(DataComparison)).Show();
     }
+
+    private void ResizeScrollView()
+    {
+        GUI.DrawTexture(cursorChangeRect, EditorGUIUtility.whiteTexture);
+        EditorGUIUtility.AddCursorRect(cursorChangeRect, MouseCursor.ResizeHorizontal);
+
+        if(Event.current.type == EventType.MouseDown && cursorChangeRect.Contains(Event.current.mousePosition))
+        {
+            resize = true;
+        }
+        if (resize)
+        {
+            currentScrollViewWidth = Event.current.mousePosition.x;
+            cursorChangeRect.Set(currentScrollViewWidth, cursorChangeRect.y, cursorChangeRect.width, cursorChangeRect.height);
+        }
+        if (Event.current.type == EventType.MouseUp)
+        {
+            resize = false;
+        }
+    }
 }
 
 public class DataComparison : EditorWindow
 {
-    private static LocalizationData localizationData;
+    public LocalizationData localizationData;
 
     private void OnGUI()
     {
