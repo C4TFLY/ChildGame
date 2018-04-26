a647c81c1157ad02df544bb168d15ec4a41ca316
C4TFLY <linus.holmstrom@hotmail.com>
2018-04-26 15:51:57 +0200

    Menu

--- Assets/Scripts/Managers/MainMenuManager.cs
+++ Assets/Scripts/Managers/MainMenuManager.cs
@@ -2,9 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour
 {
+
+    public TextMeshProUGUI masterVolumeLabel;
+    public Slider masterVolumeSlider;
+
     public void QuitGame()
     {
         Debug.Log("Quitting application.");
@@ -23,5 +29,22 @@ public class MainMenuManager : MonoBehaviour
         SceneManager.LoadScene(0);
         Destroy(LocalizationManager.instance.gameObject);
     }
-	
+
+    private void Update()
+    {
+        if (masterVolumeLabel.gameObject.activeInHierarchy)
+        {
+            float roundedVal = Round(masterVolumeSlider.value, 2);
+            string roundedStr = roundedVal.ToString().Replace(",", ".");
+            
+            masterVolumeLabel.text = roundedVal == 1 ? "1.00" : roundedVal == 0 ? "0.00" : roundedStr;
+        }
+    }
+
+    private float Round(float value, int digits)
+    {
+        float mult = Mathf.Pow(10.0f, digits);
+        return Mathf.Round(value * mult) / mult;
+    }
+
 }
