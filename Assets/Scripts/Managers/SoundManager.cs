943c17ea41fc395e3edbbfe22bb88d2f9fd69f2b
C4TFLY <linus.holmstrom@hotmail.com>
2018-04-27 10:25:19 +0200

    Working on sound config

---
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundManager : MonoBehaviour {
+
+    public static SoundManager instance;
+
+    public AudioMixer mixer;
+    public float masterVolume, musicVolume, sfxVolume, uiVolume;
+
+	void Awake () {
+		if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+	}
+
+    public void SetMasterVolume(float value)
+    {
+        masterVolume = value;
+        mixer.
+    }
+	
+}
