d8495ecf93c9484eca9074c48037ed0e51171dc6
C4TFLY <linus.holmstrom@hotmail.com>
2018-04-27 14:32:49 +0200

    Sound management. Mute/Unmute is WIP

--- Assets/Scripts/Managers/MainMenuManager.cs
+++ Assets/Scripts/Managers/MainMenuManager.cs
@@ -7,8 +7,6 @@ using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour
 {
-
-    public TextMeshProUGUI masterVolumeLabel;
     public Slider masterVolumeSlider;
 
     public void QuitGame()
@@ -28,23 +26,6 @@ public class MainMenuManager : MonoBehaviour
         Debug.Log("Going back to language selection.");
         SceneManager.LoadScene(0);
         Destroy(LocalizationManager.instance.gameObject);
-    }
-
-    private void Update()
-    {
-        if (masterVolumeLabel.gameObject.activeInHierarchy)
-        {
-            float roundedVal = Round(masterVolumeSlider.value, 2);
-            string roundedStr = roundedVal.ToString().Replace(",", ".");
-            
-            masterVolumeLabel.text = roundedVal == 1 ? "1.00" : roundedVal == 0 ? "0.00" : roundedStr;
-        }
-    }
-
-    private float Round(float value, int digits)
-    {
-        float mult = Mathf.Pow(10.0f, digits);
-        return Mathf.Round(value * mult) / mult;
     }
 
 }
--- Assets/Scripts/Managers/SoundManager.cs
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using TMPro;
 
 public class SoundManager : MonoBehaviour {
 
@@ -9,6 +10,8 @@ public class SoundManager : MonoBehaviour {
 
     public AudioMixer mixer;
     public float masterVolume, musicVolume, sfxVolume, uiVolume;
+
+    private GameObject latestUpdated;
 
 	void Awake () {
 		if (instance == null)
@@ -25,7 +28,67 @@ public class SoundManager : MonoBehaviour {
     public void SetMasterVolume(float value)
     {
         masterVolume = value;
-        mixer.
+        mixer.SetFloat("masterVolume", value);
     }
-	
+
+    public void SetMusicVolume(float value)
+    {
+        musicVolume = value;
+        mixer.SetFloat("musicVolume", value);
+    }
+
+    public void SetSFXVolume(float value)
+    {
+        sfxVolume = value;
+        mixer.SetFloat("sfxVolume", value);
+    }   
+
+    public void SetUIVolume(float value)
+    {
+        uiVolume = value;
+        mixer.SetFloat("uiVolume", value);
+    }
+
+    public void UpdateMasterLabel(TextMeshProUGUI label)
+    {
+        float roundedVal = Round(masterVolume, 2);
+        string roundedStr = roundedVal.ToString().Replace(",", ".");
+
+        label.text = roundedVal == 0 ? "0.00" : roundedVal == -80 ? "-80.00" : roundedStr;
+    }
+    public void UpdateSFXLabel(TextMeshProUGUI label)
+    {
+        float roundedVal = Round(sfxVolume, 2);
+        string roundedStr = roundedVal.ToString().Replace(",", ".");
+
+        label.text = roundedVal == 0 ? "0.00" : roundedVal == -80 ? "-80.00" : roundedStr;
+    }
+
+    public void UpdateUILabel(TextMeshProUGUI label)
+    {
+        float roundedVal = Round(uiVolume, 2);
+        string roundedStr = roundedVal.ToString().Replace(",", ".");
+
+        label.text = roundedVal == 0 ? "0.00" : roundedVal == -80 ? "-80.00" : roundedStr;
+    }
+
+    public void UpdateMusicLabel(TextMeshProUGUI label)
+    {
+        float roundedVal = Round(musicVolume, 2);
+        string roundedStr = roundedVal.ToString().Replace(",", ".");
+
+        label.text = roundedVal == 0 ? "0.00" : roundedVal == -80 ? "-80.00" : roundedStr;
+    }
+
+    public void ToggleSound()
+    {
+        mixer.ClearFloat
+    }
+
+    private float Round(float value, int digits)
+    {
+        float mult = Mathf.Pow(10.0f, digits);
+        return Mathf.Round(value * mult) / mult;
+    }
+
 }
